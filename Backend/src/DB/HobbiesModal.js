479cf0a2e6b83ab23d7d92c734275342646f8d38
Nakul Sharma <nakulsharmas2005@gmail.com>
2025-03-14 00:03:55 +0530

    starting api started

---
+++ Backend/src/DB/HobbiesModal.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+
+const Hobbies = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  hobbyName: { type: String, required: true },
+  type: { type: String, enum: ["streak", "progress"], required: true },
+  subtype: { type: String },
+  streakCount: { type: Number, default: 0 },
+  lastCompleted: { type: Date },
+  progress: {
+    unit: { type: String },
+    totalGoal: { type: Number },
+    currentProgress: { type: Number, default: 0 },
+    goal: { type: String },
+    startDate: { type: Date, default: Date.now },
+  },
+  created_at: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Hobbies", Hobbies);
+
