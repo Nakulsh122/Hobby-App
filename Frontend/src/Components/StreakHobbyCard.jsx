71e4bb6a1845d7a0e3a0c9e39182564e09a770b2
Nakul Sharma <nakulsharmas2005@gmail.com>
2025-03-19 22:40:16 +0530

    add hobby streak

--- Frontend/src/Components/StreakHobbyCard.jsx
+++ Frontend/src/Components/StreakHobbyCard.jsx
@@ -1,19 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Trash } from 'lucide-react';
+import { useEffect } from 'react';
 
-const STREAKHOBBYCARD = ({data}) => {
+const STREAKHOBBYCARD = ({data,sendData}) => {
+  const [modal ,setmodal] = useState(false);
+  const [hobbydata,setHobbydata] = useState({});
+  const datediff =(curr,last)=>{
+    if(curr.split("T")[0] === last.split("T")[0]){
+      return false
+    }
+    return true;
+  }
+  const addstreak = () =>{
+    // console.log(hobbydata)
+    var hobby = {...hobbydata};
+    // console.log(hobby);
+    // console.log("steak",hobby.streakCount);
+    const currtime = new Date();
+    const todayDate = new Date().toISOString();
+    const lastupdatedtime = new Date(hobby.lastupdated);
+    const lastupdatedDate = new Date(lastupdatedtime).toISOString();
+    let interval = currtime - lastupdatedtime;;
+    // console.log("interval",interval);
+    if(interval > 86400000){
+      hobby.streakCount = 1;
+      hobby.lastupdated = new Date().toISOString();
+      console.log("reset")
+    }else if(datediff(todayDate,lastupdatedDate)){
+      hobby.streakCount++;
+      hobby.lastupdated = new Date().toISOString()
+      console.log("Increament");  
+    }else{
+      console.log("Already updated for today")
+    }
+    setHobbydata(hobby);
+    sendData(hobby);
+  }
+  useEffect(()=>{
+    setHobbydata(data);
+  },[data])
+
   return (
     <> 
     <div className='flex-col space-y-5 font-mono bg-slate-100 w-64  rounded-md p-3'>
       <div >
-      <h1 className='font-mono font-bold text-lg'>{data.hobbyName}</h1>
-      <p>Date Started : {data.created_at}</p>
-      <p>Longest Streak : {data.streakCount}</p>
-      <p>Current Streak : {data.streakCount}</p>
+      <h1 className='font-mono font-bold text-lg'>{hobbydata.hobbyName}</h1>
+      <p>Date Started : {hobbydata.created_at}</p>
+      <p>Longest Streak : {hobbydata.streakCount}</p>
+      <p>Current Streak : {hobbydata.streakCount}</p>
       </div>
       <div className='flex-col space-y-2'>
 
-<button className='w-[100%] bg-blue-600 text-white font-bold py-2 rounded-md hover:opacity-90 active:opacity-95'>Add Streak</button>  
+<button className='w-[100%] bg-blue-600 text-white font-bold py-2 rounded-md hover:opacity-90 active:opacity-95' onClick={addstreak}>Add Streak</button>  
 <button className='w-[100%] bg-red-600 text-white font-bold py-2 rounded-md hover:opacity-90 active:opacity-95 flex items-center justify-center'><Trash className='size-5 '/><span>Delete</span></button>  
   </div> 
     </div>
--- Frontend/src/Pages/HOMEPAGE.jsx
+++ Frontend/src/Pages/HOMEPAGE.jsx
@@ -12,27 +12,47 @@ const hobbies_url_base = "http://localhost:5000/api/v1/hobby"
 //main homepage function
 const HOMEPAGE = () => {
   const [hobbies,setHobbies] = useState([]);
-  const {id} = jwtDecode(localStorage.getItem('Token'));
   const [hobbyModal , setHobbyModal] = useState(false);
   
   const getHobbiesData = async()=>{
     const {id} = jwtDecode(localStorage.getItem('Token'));
     const data = await callAPI(`${hobbies_url_base}/${id}`,"GET",undefined,undefined,localStorage.getItem("Token"));
-    setHobbies(data);
+    // console.log(data)
+    if(data.length > 0){
+      setHobbies(data);
+    }
+
   }
   const closeModal = ()=>{
     setHobbyModal(false)
   }
   const addHobby = async(hobbydata)=>{
-    console.log(hobbydata)
+    // console.log(hobbydata)
     const {id} = jwtDecode(localStorage.getItem("Token"))
-    const data = {...hobbydata,userId : id}
+    const data = {...hobbydata,userId : id};
     const response = await callAPI(`${hobbies_url_base}`,"POST",data,undefined,localStorage.getItem("Token"))
     getHobbiesData();
   }
+
   useEffect(()=>{
     getHobbiesData();
+    // console.log(hobbies);
   },[])
+
+  const deleteHobby = (id)=>{
+
+  }
+
+  const addprogress = (id)=>{
+
+  }
+  const handleUpdate = async (hobbydata)=>{
+    // console.log(hobbydata)
+    const response = await callAPI(`${hobbies_url_base}/${hobbydata._id}`,"PUT",hobbydata,undefined,localStorage.getItem("Token"))
+    getHobbiesData();
+    // console.log(hobbies)
+  }
+
   return (
     <div>
       {hobbyModal && <HOBBYMODAL isOpen={hobbyModal} onClose={closeModal} onSave={addHobby}/>}
@@ -47,13 +67,10 @@ const HOMEPAGE = () => {
       <div className="w-3/4 p-4 text-center">
         <h2 className="text-2xl font-semibold mb-4">Your Hobbies</h2>
         <button className='p-2 m-auto bg-blue-500 rounded-md justify-center items-center text-white flex hover:opacity-90 active:opacity-95'onClick={()=> setHobbyModal(true)}><PlusCircleIcon className='size-5 mx-1'/><span>Add Hobbies</span></button>
-        <div className="grid grid-cols-3 gap-4">
-          { if(hobbies.len > 0){
-
-            hobbies.map(hobby =>
-              hobby.type === "progress" ? <PROGRESSHOBBYCARD key={hobby._id} data={hobby}/> : <STREAKHOBBYCARD key={hobby._id} data={hobby}/>
-            )}
-          } 
+        <div className="grid grid-cols-3 gap-4 overflow-y-scroll">
+          {hobbies.map(hobby =>(
+            hobby.type === "progress" ? <PROGRESSHOBBYCARD key={hobby._id} data = {hobby} /> : <STREAKHOBBYCARD key={hobby._id} data={hobby} sendData={handleUpdate}/>
+          ))} 
         </div>
       </div>
     </div>
